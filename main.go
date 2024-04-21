4c924f0a992f8bb98e651030f2042b4e62050fbb
Dao Pham <daopm.dean@gmail.com>
2024-04-22 00:25:14 +0700

    get & create

--- main.go
+++ main.go
@@ -1,7 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/daopmdean/go-mysql-crud/model"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("mysql crud")
+	dsn := "root:password@tcp(127.0.0.1:3306)/gorm_test?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn))
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(model.Book{})
+
+	r := gin.Default()
+
+	r.GET("/books", func(ctx *gin.Context) {
+		var books []*model.Book
+		db.Find(&books)
+		ctx.JSON(http.StatusOK, gin.H{
+			"books": books,
+		})
+	})
+
+	r.GET("/books/:id", func(ctx *gin.Context) {
+		idStr := ctx.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			ctx.JSON(http.StatusOK, gin.H{
+				"error": "invalid id",
+			})
+			return
+		}
+
+		var book model.Book
+		db.First(&book, id)
+		ctx.JSON(http.StatusOK, gin.H{
+			"book": book,
+		})
+	})
+
+	r.POST("/books", func(ctx *gin.Context) {
+		book := model.Book{}
+		if err := ctx.ShouldBindJSON(&book); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "failed binding json",
+			})
+			return
+		}
+
+		if book.Name == "" {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "book name required",
+			})
+			return
+		}
+
+		tx := db.Create(&book)
+		if tx.Error != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "failed create book",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"id":     book.ID,
+			"create": "ok",
+		})
+	})
+
+	r.Run()
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,29 @@
 # go-mysql-crud
+
+## Init project commands
+
+```
+go mod init github.com/daopmdean/go-mysql-crud
+```
+
+git
+
+```
+git init
+
+git remote add origin git@github.com:daopmdean/go-mysql-crud.git
+
+git push -u origin main
+```
+
+```
+go get -u github.com/gin-gonic/gin
+
+go get -u gorm.io/gorm
+
+go get -u gorm.io/driver/mysql
+```
+
+```
+go mod tidy
+```
